c0c1d86ec585ebe098c744b72b82a64224d8dab6
r0nsha <r0nsh4v1t@gmail.com>
2025-05-22 00:38:49 +0300

    tap dances

--- keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
+++ keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
@@ -18,10 +18,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "action_layer.h"
 #include "color.h"
 #include "keycodes.h"
-#include "rgblight/rgblight.h"
 #include "quantum_keycodes.h"
+#include "rgb_matrix.h"
 #include QMK_KEYBOARD_H
 
 // TODO: layer: colemak
@@ -40,21 +41,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layers {
     QWERTY,
     COLEMAK,
-    EXT,
-    SYM,
-    NUM,
-    FUN,
+    /* EXT, */
+    /* SYM, */
+    /* NUM, */
+    /* FUN, */
     GAMING,
 };
 
 /* enum custom_keycodes {}; */
 
+enum tap_dances {
+    TD_GAMING,
+    TD_COLEMAK,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_COLEMAK] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_NO, COLEMAK),
+    [TD_GAMING]  = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_NO, GAMING),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_split_3x6_3(
-        UG_TOGG, KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,             QK_BOOT,
-        UG_NEXT, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_QUOTE), QK_RBT,
-        UG_PREV, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          EE_CLR,
-                                             KC_ESC,       KC_SPC,       KC_TAB, KC_ENT, KC_BSPC,      KC_TAB
+        QK_BOOT,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,             QK_BOOT,
+        EE_CLR,         LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_QUOTE), RM_NEXT,
+        TD(TD_COLEMAK), KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          TD(TD_GAMING),
+                                                    KC_ESC,       KC_SPC,       KC_TAB, KC_ENT, KC_BSPC,      KC_TAB
+    ),
+
+    [COLEMAK] = LAYOUT_split_3x6_3(
+        _______, KC_Q, KC_W, KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,   KC_QUOT, _______,
+        _______, KC_A, KC_R, KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    _______,
+        _______, KC_Z, KC_X, KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, _______,
+                             _______, _______, _______, _______, _______, _______
+    ),
+
+    [GAMING] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,    KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,             _______,
+        KC_LCTL, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,    KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_QUOTE), _______,
+        KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,    KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          _______,
+                                             KC_ESC,       KC_SPC,       KC_LALT, KC_ENT, KC_BSPC,      KC_TAB
     ),
 };
 
@@ -97,16 +122,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     return true;
 }
 
-/* layer_state_t layer_state_set_user(layer_state_t state) { */
-/*     switch (get_highest_layer(state)) { */
-/*         case QWERTY: */
-/*         default: */
-/*             rgblight_setrgb(RGB_WHITE); */
-/*             break; */
-/*     } */
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+}
 
-/*     return state; */
-/* } */
+layer_state_t default_layer_set_user(layer_state_t state) {
+    return layer_state_set_user(state);
+}
+
+static const hsv_t layer_colors[] = {
+    [QWERTY]  = {HSV_ORANGE},
+    [COLEMAK] = {HSV_MAGENTA},
+    [GAMING]  = {HSV_RED},
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = get_highest_layer(state);
+
+    hsv_t hsv = layer_colors[QWERTY];
+    if (layer < sizeof(layer_colors) / sizeof(layer_colors[0])) {
+        hsv = layer_colors[layer];
+    }
+
+    rgb_matrix_sethsv(hsv.h, hsv.s, hsv.v);
+    return state;
+}
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
