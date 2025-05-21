55d157adfdec84eddce5f60d655a112b6cd5d056
r0nsha <r0nsh4v1t@gmail.com>
2025-05-21 15:36:24 +0300

    feat: home row mods

--- keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
+++ keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
 enum layers {
@@ -49,10 +50,10 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_split_3x6_3(
-        KC_NO, KC_Q,         KC_W, KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,     KC_NO,
-        KC_NO, LGUI_T(KC_A), KC_S, KC_D,   KC_F,   KC_G,   KC_H,   KC_J,     KC_K,    KC_L,   KC_QUOTE, KC_NO,
-        KC_NO, KC_Z,         KC_X, KC_C,   KC_V,   KC_B,   KC_N,   KC_M,     KC_COMM, KC_DOT, KC_SLSH,  KC_NO,
-                                   KC_ESC, KC_SPC, KC_TAB, KC_ENT, BSPC_DEL, KC_TAB
+        KC_NO, KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,             KC_NO,
+        KC_NO, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_QUOTE), KC_NO,
+        KC_NO, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_NO,
+                                           KC_ESC,       KC_SPC,       KC_TAB, KC_ENT, BSPC_DEL,     KC_TAB
     ),
 };
 
