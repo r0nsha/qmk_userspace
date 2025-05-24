5fd563a1dd7dcda6a01c67cb3ac56121f618cd6f
r0nsha <r0nsh4v1t@gmail.com>
2025-05-24 23:24:36 +0300

    remove hrm from gaming layer

--- keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
+++ keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [GAMING] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,    KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,             _______,
-        KC_LCTL, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,    KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_QUOTE), _______,
-        KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,    KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          _______,
-                                             KC_ESC,       KC_SPC,       KC_LALT, KC_ENT, KC_BSPC,      KC_TAB
+        KC_TAB,  KC_Q, KC_W, KC_E,   KC_R,   KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,     _______,
+        KC_LCTL, KC_A, KC_S, KC_D,   KC_F,   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,   KC_QUOTE, _______,
+        KC_LSFT, KC_Z, KC_X, KC_C,   KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  _______,
+                             KC_ESC, KC_SPC, KC_LALT, KC_ENT, KC_BSPC, KC_TAB
     ),
 };
 
