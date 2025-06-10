8d2d1712bbd0a585aef1d141840301a9c6129851
r0nsha <r0nsh4v1t@gmail.com>
2025-06-11 00:01:13 +0300

    add num and fun layers to gaming layer

--- keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
+++ keyboards/crkbd/rev1/keymaps/r0nsha/keymap.c
@@ -35,19 +35,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layers {
     QWERTY,
     COLEMAK,
+    GAMING,
     EXT,
     SYM,
     NUM,
     FUN,
-    GAMING,
 };
 
 static const hsv_t layer_colors[] = {
-    [QWERTY] = {HSV_SPRINGGREEN}, [COLEMAK] = {HSV_MAGENTA}, [EXT] = {HSV_PINK}, [SYM] = {HSV_ORANGE}, [NUM] = {HSV_BLUE}, [FUN] = {HSV_PURPLE}, [GAMING] = {HSV_RED},
+    [QWERTY] = {HSV_SPRINGGREEN}, [COLEMAK] = {HSV_MAGENTA}, [GAMING] = {HSV_RED}, [EXT] = {HSV_PINK}, [SYM] = {HSV_ORANGE}, [NUM] = {HSV_BLUE}, [FUN] = {HSV_PURPLE},
 };
 
 static const char* layer_names[] = {
-    [QWERTY] = "QWERTY", [COLEMAK] = "COLEMAK", [EXT] = "EXT", [SYM] = "SYM", [NUM] = "NUM", [FUN] = "FUN", [GAMING] = "GAMING",
+    [QWERTY] = "QWERTY", [COLEMAK] = "COLEMAK", [GAMING] = "GAMING", [EXT] = "EXT", [SYM] = "SYM", [NUM] = "NUM", [FUN] = "FUN",
 };
 
 enum custom_keycodes {
@@ -81,6 +81,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              _______, _______, _______, _______, _______, _______
     ),
 
+    [GAMING] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q, KC_W, KC_E,            KC_R,   KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,     _______,
+        KC_LCTL, KC_A, KC_S, KC_D,            KC_F,   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,   KC_QUOTE, _______,
+        KC_LSFT, KC_Z, KC_X, KC_C,            KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  _______,
+                             LT(NUM, KC_ESC), KC_SPC, KC_LALT, KC_ENT, KC_BSPC, MO(FUN)
+    ),
+
     [EXT] = LAYOUT_split_3x6_3(
         _______, XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ESC, _______,
         _______, LGUI(XXXXXXX), LALT(XXXXXXX), LCTL(XXXXXXX), LSFT(XXXXXXX), XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, _______,
@@ -107,13 +114,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RM_NEXT, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRIU, XXXXXXX, KC_F11,  KC_F4,  KC_F5, KC_F6, QK_RBT,
         RM_PREV, KC_PSCR, KC_VOLD, KC_MUTE, KC_VOLU, KC_BRID, XXXXXXX, KC_F10,  KC_F1,  KC_F2, KC_F3, EE_CLR,
                                    _______, _______, _______, _______, _______, _______
-    ),
-
-    [GAMING] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q, KC_W, KC_E,   KC_R,   KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,     _______,
-        KC_LCTL, KC_A, KC_S, KC_D,   KC_F,   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,   KC_QUOTE, _______,
-        KC_LSFT, KC_Z, KC_X, KC_C,   KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  _______,
-                             KC_ESC, KC_SPC, KC_LALT, KC_ENT, KC_BSPC, KC_TAB
     ),
 };
 
